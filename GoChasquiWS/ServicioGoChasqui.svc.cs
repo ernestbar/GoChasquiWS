803aff33d0d049015e4a11edadd975d3789ddbd5
Eric Muñoz <ericfmb2012@gmail.com>
2020-10-21 15:17:58 -0400

    Agregar archivos de proyecto.

---
+++ GoChasquiWS/ServicioGoChasqui.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GoChasquiWS
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioGoChasqui" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ServicioGoChasqui.svc o ServicioGoChasqui.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ServicioGoChasqui : IServicioGoChasqui
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
